f6067728e066e26e9bdf961b7efacd1d230f34ac
Shaoranlaos <shaoranlaos@shaoranlaos.de>
2018-03-14 19:55:24 +0100

    create test Configuration

--- src/main/java/de/shaoranlaos/scm_backup_plugin/BackupConfiguration.java
+++ src/main/java/de/shaoranlaos/scm_backup_plugin/BackupConfiguration.java
@@ -2,4 +2,13 @@ package de.shaoranlaos.scm_backup_plugin;
 
 public class BackupConfiguration {
 
+	private String backupScriptPath;
+
+	public String getBackupScriptPath() {
+		return backupScriptPath;
+	}
+
+	public void setBackupScriptPath(String backupScriptPath) {
+		this.backupScriptPath = backupScriptPath;
+	}
 }
